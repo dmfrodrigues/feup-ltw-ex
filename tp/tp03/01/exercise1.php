ad8c3a46d56b2361914ab0c7629dddf512398ec6
Diogo Miguel Ferreira Rodrigues <up201806429@feup-b101d11.fe.up.pt>
2020-10-16 11:02:36 +0100

    Done with TP03 up to the first half of ex5

---
+++ tp/tp03/01/exercise1.php
@@ -0,0 +1,5 @@
+<?php echo "<h1>Hello World!</h1>"?>
+<h1>
+<?php echo "Hello World!" ?>
+</h1>
+<h1><?= "Hello World!" ?></h1>
---
+++ tp/tp03/02/sum2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <p>
+<?php
+
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+
+echo $num1 + $num2;
+
+?>
+    </p>
+    <p>
+        <a href="form2.html">Back to form</a>
+    </p>
+</body>
+</html>
