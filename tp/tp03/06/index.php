9461dae23db06304d7a4a5e702dfea333a2b7ac7
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-23 12:09:27 +0100

    Created list_news.php

--- tp/tp03/06/index.php
+++ tp/tp03/06/index.php
@@ -21,36 +21,9 @@ $articles = getAllNews();
     <?php
     include_once('templates/common/header.php');
     ?>
-    <section id="news">
-      <?php
-      foreach($articles as $article) {
-        $epoch = (int)$article['published'];
-        $dt = new DateTime("@$epoch");
-      ?>
-      <article>
-        <header>
-          <h1><a href="item.php?id=<?=$article['id']?>"><?=$article['title']?></a></h1>
-        </header>
-        <img src="http://lorempixel.com/600/300/business/" alt="">
-        <p><?=$article['introduction']?></p>
-        <?php
-        echo implode(' ', array_map(function($tag){ return '<p>'.$tag.'</p>'; }, explode(PHP_EOL, $article['fulltext'])));
-        ?>
-        <footer>
-          <span class="author"><?=$article['name']?></span>
-          <span class="tags">
-          <?php
-          echo implode(' ', array_map(function($tag){ return '<a href=\'index.php\'>#'.$tag.'</a>'; }, explode(',', $article['tags'])));
-          ?>
-          </span>
-          <span class="date"><?=$dt->format('Y-m-d, H:i')?></span>
-          <a class="comments" href="item.php?id=<?=$article['id']?>#comments\"><?=$article['comments']?></a>
-        </footer>
-      </article>
-      <?php
-      }
-      ?>
-    </section>
+    <?php
+    include_once('templates/news/list_news.php');
+    ?>
     <?php
     include_once('templates/common/footer.php');
     ?>
