10a430a05b4ce24e4035159388a2224ff22f5391
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-23 11:29:23 +0100

    Created database/news

--- tp/tp03/06/index.php
+++ tp/tp03/06/index.php
@@ -1,15 +1,8 @@
 <?php
 
 include_once('database/connection.php');
-$stmt = $db->prepare('SELECT news.*, users.*, COUNT(comments.id) AS comments
-FROM news JOIN
-     users USING (username) LEFT JOIN
-     comments ON comments.news_id = news.id
-GROUP BY news.id, users.username
-ORDER BY published DESC
-');
-$stmt->execute();
-$articles = $stmt->fetchAll();
+include_once('database/news.php');
+$articles = getAllNews();
 
 ?>
 <!DOCTYPE html>
