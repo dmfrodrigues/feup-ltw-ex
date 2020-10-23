18ee833332214455021065c783a29292ccb72430
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-23 10:38:42 +0100

    Changed item.php

--- tp/tp03/03/item.php
+++ tp/tp03/03/item.php
@@ -48,41 +48,47 @@
       </article>
     </aside>
     <section id="news">
+      <?php
+      $db = new PDO('sqlite:news.db');
+      $stmt = $db->prepare('SELECT * FROM news JOIN users USING (username) WHERE id = :id');
+      $stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+      $stmt->execute();
+      $article = $stmt->fetch();
+
+      $epoch = (int)$article['published'];
+      $dt = new DateTime("@$epoch");
+      ?>
+      <!-- <pre><?php var_dump($article); ?></pre> -->
+
       <article>
         <header>
-          <h1><a href="item.html">Quisque a dapibus magna, non scelerisque</a></h1>
+          <h1><a href="item.php?id=<?=$_GET['id']?>"><?=$article["title"]?></a></h1>
         </header>
         <img src="http://lorempixel.com/600/300/business/" alt="">
-        <p>Etiam massa magna, condimentum eu facilisis sit amet, dictum ac purus. Curabitur semper nisl vel libero pulvinar ultricies. Proin dignissim dolor nec scelerisque bibendum. Maecenas a sem euismod, iaculis erat id, convallis arcu. Ut mollis, justo vitae suscipit imperdiet, eros dui laoreet enim, fermentum posuere felis arcu vel urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin blandit ex sit amet suscipit commodo. Duis molestie ligula eu urna tincidunt tincidunt. Mauris posuere aliquet pellentesque. Fusce molestie libero arcu, ut porta massa iaculis sit amet. Fusce varius nisl vitae fermentum fringilla. Pellentesque a cursus lectus.</p>
-          <p>Duis condimentum metus et ex tincidunt, faucibus aliquet ligula porttitor. In vitae posuere massa. Donec fermentum magna sit amet suscipit pulvinar. Cras in elit sapien. Vivamus nunc sem, finibus ac suscipit ullamcorper, hendrerit vitae urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque eget tincidunt orci. Mauris congue ipsum non purus tristique, at venenatis elit pellentesque. Etiam congue euismod molestie. Mauris ex orci, lobortis a faucibus sed, sagittis eget neque.</p>
-          <p>Mauris tincidunt orci congue turpis viverra pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque rhoncus lorem eget.</p>
+        <p><?=$article['introduction']?></p>
+        <?php
+        echo implode(' ', array_map(function($tag){ return '<p>'.$tag.'</p>'; }, explode(PHP_EOL, $article['fulltext'])));
+        ?>    
         <section id="comments">
-          <h1>5 Comments</h1>
+          <?php
+          $stmt = $db->prepare('SELECT * FROM comments JOIN users USING (username) WHERE news_id = ?');
+          $stmt->execute(array($_GET['id']));
+          $comments = $stmt->fetchAll();
+          ?>
+          <h1><?=count($comments)?> Comments</h1>
+          <?php
+          foreach($comments as $comment) {
+            $epoch_comment = (int)$comment['published'];
+            $dt_comment = new DateTime("@$epoch_comment");
+          ?>
           <article class="comment">
-            <span class="user">updatespeak</span>
-            <span class="date">1m</span>
-            <p>Aliquam maximus commodo dui, ut viverra urna vulputate et. Donec posuere vitae sem sed vehicula. Sed in erat eu diam fringilla sodales. Aenean lacinia vulputate nisl, dignissim dignissim nisl. Nam at nibh mollis, facilisis nibh sit amet, mattis urna. Maecenas.</p>
+            <span class="user"><?=$comment['username']?></span>
+            <span class="date"><?=$dt_comment->format('Y-m-d, H:i')?></span>
+            <p><?=$comment['text']?></p>
           </article>
-          <article class="comment">
-            <span class="user">distortedscorpius</span>
-            <span class="date">3m</span>
-            <p>Duis scelerisque purus fermentum turpis euismod congue. Phasellus sit amet sem mollis, imperdiet quam porta, volutpat purus. In et sodales urna, sed cursus lectus. Vivamus a massa vitae nisl lobortis laoreet nec tristique magna. Mauris egestas ipsum eu sem lacinia.</p>
-          </article>
-          <article class="comment">
-            <span class="user">duplicateengineer</span>
-            <span class="date">7m</span>
-            <p>Phasellus at neque nec nunc scelerisque eleifend eu eu risus. Praesent in nibh viverra, posuere ligula condimentum, accumsan tellus. Vivamus varius sem a mauris finibus, ac iaculis risus scelerisque. Nullam fermentum leo dui, at fermentum tellus consequat id. Pellentesque eleifend.</p>
-          </article>
-          <article class="comment">
-            <span class="user">charityheinz</span>
-            <span class="date">9m</span>
-            <p>Nam at elit ut orci viverra viverra vitae dictum sapien. Morbi sed eleifend eros. Nunc fermentum, nulla id vehicula posuere, justo orci commodo urna, a blandit orci massa vitae urna. Sed commodo sollicitudin quam. Suspendisse molestie eget libero nec finibus.</p>
-          </article>
-          <article class="comment">
-            <span class="user">plutoniumfogg</span>
-            <span class="date">11m</span>
-            <p>Aliquam dignissim finibus lectus non condimentum. Cras accumsan diam vitae nulla efficitur congue. Vivamus porta arcu sit amet dapibus ultricies. Donec ac sodales mauris. Nulla eget tortor urna. Donec a malesuada libero. Curabitur blandit erat ut diam rhoncus venenatis. Proin.</p>
-          </article>
+          <?php
+          }
+          ?>
           <form>
             <h2>Add your voice...</h2>
             <label>Username 
@@ -98,10 +104,14 @@
           </form>
         </section>
         <footer>
-          <span class="author">Dominic Woods</span>
-          <span class="tags"><a href="index.php">#politics</a> <a href="index.php">#economy</a></span>
-          <span class="date">15m</span>
-          <a class="comments" href="item.html#comments">5</a>
+        <span class="author"><?=$article['name']?></span>
+          <span class="tags">
+          <?php
+          echo implode(' ', array_map(function($tag){ return '<a href=\'index.php\'>#'.$tag.'</a>'; }, explode(',', $article['tags'])));
+          ?>
+          </span>
+          <span class="date"><?=$dt->format('Y-m-d, H:i')?></span>
+          <a class="comments" href="item.php?id=<?=$_GET['id']?>#comments\"><?=$article['comments']?></a>
         </footer>
       </article>
     </section>
