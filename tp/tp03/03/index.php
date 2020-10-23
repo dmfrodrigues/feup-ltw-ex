9614e9accef1df9dd9d81f89b3ff2846b745002e
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-23 10:39:12 +0100

    Changed html to php

--- tp/tp03/03/index.php
+++ tp/tp03/03/index.php
@@ -29,8 +29,8 @@ $articles = $stmt->fetchAll();
       <h1><a href="index.php">Super Legit News</a></h1>
       <h2><a href="index.php">Where fake news are born!</a></h2>
       <div id="signup">
-        <a href="register.html">Register</a>
-        <a href="login.html">Login</a>
+        <a href="register.php">Register</a>
+        <a href="login.php">Login</a>
       </div>
     </header>
     <nav id="menu">
