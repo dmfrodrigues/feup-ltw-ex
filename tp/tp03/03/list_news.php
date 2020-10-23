a9e92cc17c471802c8ce397ec6acb1ed03cea9bb
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-23 09:59:29 +0100

    Fixed issue related to sqlite queries

--- tp/tp03/03/list_news.php
+++ tp/tp03/03/list_news.php
@@ -1,8 +1,7 @@
 <?php
 
 $db = new PDO('sqlite:news.db');
-$stmt = $db->prepare('
-SELECT news.*, users.*, COUNT(comments.id) AS comments
+$stmt = $db->prepare('SELECT news.*, users.*, COUNT(comments.id) AS comments
 FROM news JOIN
      users USING (username) LEFT JOIN
      comments ON comments.news_id = news.id
